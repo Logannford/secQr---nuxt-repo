19c10315272c335274d27b5b5cd02273c46bae76
Logan Ford <logan@hiyield.co.uk>
2023-09-13 17:00:11 +0100

    pricing card style changes

--- pages/pricing/index.vue
+++ pages/pricing/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div 
     class="
-      text-white h-screen w-full flex flex-col gap-y-12
-      justify-center items-center bg-white/95
+      text-white h-full lg:h-screen w-full flex flex-col gap-y-12
+      justify-center items-center bg-white/95 inside-container
     "
   >
     <div class="flex flex-col items-center gap-y-3 text-black">
-      <h6 class="bg-white/30 px-4 py-2 rounded-xl text-xs">
+      <h6 class="bg-light-purple/20 px-4 py-2 rounded-xl text-xs text-white">
         Pricing
       </h6>
       <h1 class="text-5xl font-bold">
@@ -14,7 +14,10 @@
       </h1>
     </div>
     <div 
-      class="grid grid-cols-3 gap-x-3 w-3/5 items-end h-4/6"
+      class="
+        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-5 
+        w-[65%] items-end h-full lg:h-4/6
+      "
     >
       <PaymentCard 
         v-for="(item, index) in itemOptions"
@@ -25,6 +28,7 @@
         :price="item.price"
         :bulletPoints="item.bulletPoints"
         :mostPopular="item.mostPopular"
+        :index="index"
       />
     </div>
   </div>
@@ -64,7 +68,8 @@ const itemOptions: ItemOptions[] = [
     shortDescription: "1 Time Purchase",
     bulletPoints: [
       "Allows for 1 Address QR code to be generated.",
-      "Can be used once."
+      "Unlimited uses.",
+      "Can only be used to generate home address - not custom information."
     ]
   },
   {
@@ -73,10 +78,11 @@ const itemOptions: ItemOptions[] = [
     price: 499,
     shortDescription: "Monthly Subscription with unlimited QR code generation",
     bulletPoints: [
-      "ALL single time purchase features",
+      "ALL single time purchase features.",
       "Allows for multiple Address QR codes to be generated.",
       "Can be used unlimited times.",
       "Can track all orders from the dashboard for the month.",
+      "Can generate QR codes with custom information."
     ],
     mostPopular: true
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,15 +22,24 @@ module.exports = {
 			}
 		},
     extend: {
+      animation: {
+        text: 'text 5s ease infinite',
+      },
       colors: {
+        "white": "#FFFFFF",
+        "black": "#000000",
+        "transparent": "transparent",
         "dark-black": "#0A0A0A",
         "light-black": "#1c1c1c",
         "onyx-black": "#3E3E3E",
         "light-grey": "#B6B6B6",
-        "dark-purple": "#240046"
-      },
-      animation: {
-        text: 'text 5s ease infinite',
+        "dark-purple": "#240046",
+        "light-purple": "#7b2cbf",
+
+
+        "secqr-purple": {
+            "500": "#7b2cbf"
+        },
       },
       keyframes: {
         text: {
