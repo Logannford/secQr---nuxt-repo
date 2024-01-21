c2107288a2bbcf0881e0f2cbccfc8d1ac7f1181e
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-01-21 16:24:13 +0000

    login method created + temp page

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="w-full h-screen flex flex-col justify-center items-center bg-dark-black"
+    class="w-full h-screen flex flex-col gap-y-4 justify-center items-center bg-dark-black"
   >
-    <h1 class="text-5xl font-bold">Sign up</h1>
+    <h1 class="text-3xl font-bold text-white">Login</h1>
     <form
-      @submit.prevent="handleRegistration()"
+      @submit.prevent="handleLogin()"
       class="flex flex-col gap-y-6 w-56 justify-center items-center text-white"
     >
       <div class="relative">
@@ -49,30 +49,39 @@
         :disabled="!validEmail || validPassword"
       />
     </form>
-
-    <div
-      v-if="error"
-      data-cy="error-field"
-      class="text-red-500"
-    >
-      {{ errorMessage }}
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
 import { useUserStore } from '~/stores/userStore';
+import { getAuth } from 'firebase/auth';
+
+const userStore = useUserStore();
 
 const email = ref<string>('');
 const password = ref<string>('');
-const error = ref<boolean>(false);
-const errorMessage = ref<string>('');
-const route = useRouter();
+const loading = ref<boolean>(false);
 
-const userStore = useUserStore();
-const currentUser = storeToRefs(userStore);
-const { resetUser, userSignUp } = userStore;
+const handleLogin = async () => {
+  if (!email.value || !password.value) return;
+
+  // if we have a user, sign them out
+  if (getAuth().currentUser) await userStore.signOutUser();
+
+  try {
+    // use the login method from the userStore
+    await userStore.userLoginIn(email.value, password.value);
+
+    // if we have a user, redirect them to the dashboard
+    if (getAuth().currentUser) {
+      await useRouter().push({
+        path: '/pricing',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const validEmail = computed<boolean>(() => {
   return /[!@#$%^&*(),.?":{}|<>]/.test(email.value);
@@ -81,41 +90,4 @@ const validEmail = computed<boolean>(() => {
 const validPassword = computed<boolean>(() => {
   return password.value.length < 8;
 });
-
-const loading = ref<boolean>(false);
-
-const handleRegistration = async () => {
-  // 100% clear out the current user on the sign up page,
-  // we do this in 'app.vue' on 'sign-up' route change
-  // this is just to triple check
-  if (currentUser) resetUser();
-
-  loading.value = true;
-
-  // Show loading spinner for a minimum of 2 seconds
-  const signUpPromise = userSignUp(email.value, password.value);
-
-  // Set a minimum timeout of 2 seconds
-  const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 2000));
-
-  // Wait for both the timeout and sign-up promise to complete
-  await Promise.all([timeoutPromise, signUpPromise]);
-
-  const signUpResult = await signUpPromise;
-
-  // Check if sign-up was successful or not
-  // sign up result will be true if successful, otherwise the error message as string
-  if (signUpResult !== true) {
-    error.value = true;
-    errorMessage.value = signUpResult as string;
-  } else
-    route.push({
-      path: 'pricing',
-      force: true,
-      replace: false,
-    });
-
-  //reset the loading state
-  loading.value = false;
-};
 </script>
