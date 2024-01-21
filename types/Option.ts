08ca05ce50b51d541a1fdbdba61f07a6851137ec
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-01-21 17:13:39 +0000

    adds some minor comments to ~/types/option.ts

--- types/Option.ts
+++ types/Option.ts
@@ -6,6 +6,8 @@
 export type Option<T> = { kind: 'none' } | { kind: 'some'; value: T };
 
 // Define a function 'none' that returns an Option with kind 'none'
+// the functions return type is Option<T> - so we know that the return value
+// will be an object with a kind property of type 'none' and not any string value
 export function none<T>(): Option<T> {
   return { kind: 'none' };
 }
